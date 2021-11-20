2fdfcd9275f0728ca5a7e1aef92ef84ab57f5d6a
Zahra <zahra.khan@blockfi.com>
2021-11-19 19:14:52 -0500

    [ASSIGNMENT] Complete values & variables assignment

---
+++ javascript-fundamentals-1/script.js
@@ -0,0 +1,12 @@
+// JavaScript Fundamentals Part 1 Exercises
+
+// LECTURE: Values and Variables
+// 1. Declarevariablescalled'country','continent'and'population'and assign their values according to your own country (population in millions)
+// 2. Logtheirvaluestotheconsole
+
+const country = 'United States'
+const continent = 'North America'
+const population = 329.5
+
+console.log(country, continent, population)
+
