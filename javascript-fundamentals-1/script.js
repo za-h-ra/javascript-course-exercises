0b519828d882215b298297b463d309a8a866f4c0
Zahra <zahra.khan@blockfi.com>
2021-11-22 17:49:47 -0500

    log data types exercise

--- javascript-fundamentals-1/script.js
+++ javascript-fundamentals-1/script.js
@@ -10,3 +10,14 @@ const population = 329.5
 
 console.log(country, continent, population)
 
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population' 'country'and'language' to the console
+
+const isIsland = false
+let language;
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
+
