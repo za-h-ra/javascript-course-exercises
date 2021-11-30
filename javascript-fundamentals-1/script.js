a79e5f303ed1e939a0eb2d506c89d80b559e3ffc
Zahra <zahra.khan@blockfi.com>
2021-11-29 22:49:21 -0500

    Lecture: Basic Operators

--- javascript-fundamentals-1/script.js
+++ javascript-fundamentals-1/script.js
@@ -6,7 +6,7 @@
 
 const country = 'United States'
 const continent = 'North America'
-const population = 329.5
+let population = 329.5
 
 console.log(country, continent, population)
 
@@ -15,7 +15,7 @@ console.log(country, continent, population)
 // 2. Log the types of 'isIsland', 'population' 'country'and'language' to the console
 
 const isIsland = false
-let language;
+let language
 console.log(typeof isIsland)
 console.log(typeof population)
 console.log(typeof country)
@@ -25,3 +25,34 @@ console.log(typeof language)
 
 language = 'English'
 console.log(typeof language)
+
+// LECTURE: Basic Operators
+// 1. If your country split in half,and each half would contain half the population, then how many people would live in each half?
+console.log('Population by half: ' + population / 2)
+
+// 2. Increase the population of your country by 1 and log the result to the console
+
+population++
+console.log(population)
+
+// 3. Finland has a population of 6 million Does your country have more people than Finland?
+
+let finlandPopulation = 6
+console.log(population > finlandPopulation)
+
+// 4. The average population of a country is 33 million people.Does your country have less people than the average country?
+
+let averagePopulation = 33
+console.log(country < averagePopulation)
+
+// 5.
+
+let description =
+  country +
+  ' is in ' +
+  continent +
+  ', and its ' +
+  population +
+  ' million people speak ' +
+  language
+console.log(description)
