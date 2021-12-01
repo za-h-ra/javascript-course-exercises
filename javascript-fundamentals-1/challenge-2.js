e73565dfec88daf54940ca85b775ec8760e69c44
Zahra <zahra.khan@blockfi.com>
2021-12-01 18:30:33 -0500

    Complete Challenge 2

--- javascript-fundamentals-1/challenge-2.js
+++ javascript-fundamentals-1/challenge-2.js
@@ -3,3 +3,22 @@
 // 1. Print a nice output to the console, saying who has the higher BMI.The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
 // 2. Use atemplate literal to include the BMI values in the outputs.Example:"Mark's BMI (28.3) is higher than John's (23.9)!"
 
+let markMass = 78
+const markHeight = 1.69
+let johnMass = 92
+const johnHeight = 1.95
+
+const markBMI = markMass / markHeight ** 2
+console.log(markBMI)
+
+const johnBMI = johnMass / johnHeight ** 2
+console.log(johnBMI)
+
+let markHigherBMI = markBMI > johnBMI
+console.log(markHigherBMI)
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`)
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+}
