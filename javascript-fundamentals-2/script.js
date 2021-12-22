c0e413b2ead7f336f77bc3d55b418b3b375d01f9
Zahra <zahra.khan@blockfi.com>
2021-12-22 18:59:20 -0500

    Exercise: Functions

---
+++ javascript-fundamentals-2/script.js
@@ -0,0 +1,9 @@
+function describeCountry(country, population, capitalCity) {
+  console.log(
+    `${country} has ${population} million people and its capital city is ${capitalCity}`
+  )
+}
+
+describeCountry('Costa Rica', 5.094, 'San Jose')
+describeCountry('Argentina', 45.38, 'Buenos Aires')
+describeCountry('Austrailia', 25.69, 'Canberra')
