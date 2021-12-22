b814d29a294e96a624339d8d9e581689e1ad7d12
Zahra <zahra.khan@blockfi.com>
2021-12-21 19:06:02 -0500

    Exercise: Ternary Operator

--- javascript-fundamentals-1/script.js
+++ javascript-fundamentals-1/script.js
@@ -112,3 +112,7 @@ switch (language) {
   default:
     console.log('Great language too!')
 }
+
+// TERNARY OPERATOR
+
+population > 33 ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`)
