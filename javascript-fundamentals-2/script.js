13cd835ed5e05c8b075291357017defbe0d05712
Zahra <zahra.khan@blockfi.com>
2021-12-22 19:01:20 -0500

    Refactor Functions exercise

--- javascript-fundamentals-2/script.js
+++ javascript-fundamentals-2/script.js
@@ -1,9 +1,8 @@
 function describeCountry(country, population, capitalCity) {
-  console.log(
-    `${country} has ${population} million people and its capital city is ${capitalCity}`
-  )
+  let string = `${country} has ${population} million people and its capital city is ${capitalCity}`
+  return string
 }
 
-describeCountry('Costa Rica', 5.094, 'San Jose')
-describeCountry('Argentina', 45.38, 'Buenos Aires')
-describeCountry('Austrailia', 25.69, 'Canberra')
+console.log(describeCountry('Costa Rica', 5.094, 'San Jose'))
+console.log(describeCountry('Argentina', 45.38, 'Buenos Aires'))
+console.log(describeCountry('Austrailia', 25.69, 'Canberra'))
