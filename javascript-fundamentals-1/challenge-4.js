8e84e2f8e58121f20b22313fc98b6001bda839f8
Zahra Khan <zahra.khan1@icloud.com>
2023-05-08 23:30:32 -0400

    [UPDATE]

--- javascript-fundamentals-1/challenge-4.js
+++
@@ -1,8 +0,0 @@
-// TEST DATA: 
-// Test for bill values 275, 40, 430
-
-let bill = 275
-let tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .20
-
-console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}`)
-
