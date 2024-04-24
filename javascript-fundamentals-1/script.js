90a76f56a44b31c8c6f97e28f951c2d187e1139b
za-h-ra <zahra.khan1@icloud.com>
2024-04-24 14:41:29 -0400

    logical operators assignment

--- javascript-fundamentals-1/script.js
+++ javascript-fundamentals-1/script.js
@@ -108,15 +108,32 @@ console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
 
 // Reflect on why we should use the === operator and type conversion in this situation.
 
-let numNeighbours = Number(
-  prompt("How many neighbor countries does your country have?")
-);
-console.log(numNeighbours);
+// let numNeighbours = Number(
+//   prompt("How many neighbor countries does your country have?")
+// );
+// console.log(numNeighbours);
 
-if (numNeighbours === 1) {
-  console.log("Only 1 border!");
-} else if (numNeighbours > 1) {
-  console.log("More than 1 border!");
+// if (numNeighbours === 1) {
+//   console.log("Only 1 border!");
+// } else if (numNeighbours > 1) {
+//   console.log("More than 1 border!");
+// } else {
+//   console.log("No borders!");
+// }
+
+// LOGICAL OPERATORS
+// Comment out the previous code so the prompt doesn't get in the way.
+
+// Let's say Sarah is looking for a new country to live in. She wants to live in a country that speaks English, has less than 50 million people and is not an island.
+
+// Write an if statement to help Sarah figure out if your country is right for her. You will need to write a condition that accounts for all of Sarah's criteria. Take your time with this, and check part of the solution if necessary.
+
+// If yours is the right country, log a strings like this 'You should live in Portugal :)'. If not, log 'Portugal does not meet your criteria :('.
+
+// Probably your country does not meet all the criteria. So go back and temporarily change some variables in order to make the condition true (unless you live in Canada :D).
+
+if (language === "English" && population < 50000000 && !isIsland) {
+  console.log(`You should live in the ${country}`);
 } else {
-  console.log("No borders!");
+  console.log(`${country} does not meet the criteria ):`);
 }
