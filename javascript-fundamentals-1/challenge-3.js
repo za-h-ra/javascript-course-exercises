ba1ae853614ea7c37c32a31914884ecdad55872e
za-h-ra <zahra.khan1@icloud.com>
2024-04-24 15:33:39 -0400

    challenge 3 bonus

--- javascript-fundamentals-1/challenge-3.js
+++ javascript-fundamentals-1/challenge-3.js
@@ -24,3 +24,27 @@ if (scoreKoalas === scoreDolphins) {
 } else {
   console.log("Koalas win the trophy!");
 }
+
+// BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks.
+
+// BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater than or equal to 100 points. Otherwise, no team wins the trophy.
+
+// TEST DATA BONUS 1: Dolphins score 97, 112, and 101. Koalas score 109, 95, 123
+// TEST DATA BONUS 2: Dolphins score 97, 112, and 101. Koalas score 109,95, and 106.
+
+const scoreDolphins1 = (97 + 112 + 101) / 3;
+const scoreKoalas1 = (109, 95, 123) / 3;
+
+if (scoreDolphins1 > scoreKoalas1 && scoreDolphins1 >= 100) {
+  console.log("Dolphins win the trophy!");
+} else if (scoreKoalas1 > scoreDolphins1 && scoreKoalas1 >= 100) {
+  console.log("Koalas win the trophy!");
+} else if (
+  scoreDolphins1 === scoreKoalas1 &&
+  scoreDolphins1 >= 100 &&
+  scoreKoalas1 >= 100
+) {
+  console.log("Both win the trophy!");
+} else {
+  console.log("No team wins the trophy.");
+}
