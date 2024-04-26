a0cf827200018543c782b3ad0a064ffd630b2450
za-h-ra <zahra.khan1@icloud.com>
2024-04-25 20:18:45 -0400

    switch statement assignment

--- javascript-fundamentals-1/script.js
+++ javascript-fundamentals-1/script.js
@@ -137,3 +137,37 @@ if (language === "English" && population < 50000000 && !isIsland) {
 } else {
   console.log(`${country} does not meet the criteria ):`);
 }
+
+// THE SWITCH STATEMENT
+// Use a switch statement to log the following string for the given language:
+
+// chinese or mandarin: 'MOST number of native speakers!';
+
+// spanish: '2nd place in number of native speakers';
+
+// english: '3rd place';
+
+// hindi: 'Number 4';
+
+// arabic: '5th most spoken language';
+
+// for all other simply log 'Great language too :D'.
+
+switch (language) {
+  case "Chinese" || "Mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "Spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "English":
+    console.log("3rd place");
+    break;
+  case "Hindi":
+    console.log("Number 4");
+    break;
+  case "Arabic":
+    console.log("5th most spoken language");
+  default:
+    console.log("Great language too :D");
+}
