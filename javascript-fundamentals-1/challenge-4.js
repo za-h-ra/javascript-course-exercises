cfcb12817faff1bccca3170535c6c4031b050899
za-h-ra <zahra.khan1@icloud.com>
2024-04-25 20:42:28 -0400

    Challenge 4

---
+++ javascript-fundamentals-1/challenge-4.js
@@ -0,0 +1,27 @@
+// Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+// Your tasks:
+
+// Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+// Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+// Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+
+// Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+
+// TEST DATA: Test with different bill values: 275, 40, and 430
+
+// HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+
+// HINT 2: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+const bill = 430;
+const billValue = bill >= 50 && bill <= 300;
+const tip = billValue ? bill * 0.15 : bill * 0.2;
+const total = bill + tip;
+console.log("DATA:", bill, tip, total);
+
+console.log(
+  `The bill was $${bill}, the tip was $${tip}, and the total value $${total}.`
+);
